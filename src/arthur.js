feaebd84b8fa57a69d2c1f618762086a954615fd
Bob Harterink <bobharterink@gmail.com>
2023-01-16 13:59:51 +0100

    push

---
+++ src/arthur.js
@@ -0,0 +1,112 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+const objectGroup = new THREE.Group();
+scene.add(objectGroup);
+
+const loader = new GLTFLoader();
+loader.load("models/Erangel_poster.glb", function (gltf) {
+  objectGroup.add(gltf.scene);
+});
+loader.load("models/Metropolis.glb", function (gltf) {
+  objectGroup.add(gltf.scene);
+});
+
+loader.load("models/MHdr_poster.glb", function (gltf) {
+  objectGroup.add(gltf.scene);
+});
+
+loader.load("models/Chain.glb", function (gltf) {
+  objectGroup.add(gltf.scene);
+});
+
+loader.load("models/PHP.glb", function (gltf) {
+  objectGroup.add(gltf.scene);
+});
+loader.load("models/printsheet.glb", function (gltf) {
+  objectGroup.add(gltf.scene);
+});
+
+loader.load("models/tape.glb", function (gltf) {
+  objectGroup.add(gltf.scene);
+});
+
+loader.load("models/Transversal.glb", function (gltf) {
+  objectGroup.add(gltf.scene);
+});
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2(99999,99999);
+
+function onPointerMove( event ) {
+
+	// calculate pointer position in normalized device coordinates
+	// (-1 to +1) for both components
+
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+  // console.log(pointer);
+  
+}
+
+
+window.addEventListener( 'pointermove', onPointerMove );
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(0, 0.2, 1);
+scene.add(directionalLight);
+
+camera.position.z = 5;
+
+controls.minDistance = 1.7
+controls.maxDistance = 8
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+
+  	// update the picking ray with the camera and pointer position
+	raycaster.setFromCamera( pointer, camera );
+
+	// calculate objects intersecting the picking ray
+	const intersects = raycaster.intersectObjects( scene.children );
+
+	for ( let i = 0; i < intersects.length; i ++ ) {
+    console.log(intersects[ i ].object)
+		intersects[ i ].object.material.color.set( 0xff0000 );
+	}
+
+  // cube.rotation.x += 0.01;
+  objectGroup.rotation.y += 0.0007;
+
+  renderer.render(scene, camera);
+}
+
+animate();
--- src/script.js
+++ src/script.js
@@ -2,39 +2,138 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import gsap from 'gsap'
 
-// Debug
-const gui = new dat.GUI()
+// loader
+const gltfLoader = new GLTFLoader()
+
+// loader2
+const gltfLoader2 = new GLTFLoader()
+
+// loader3
+const gltfLoader3 = new GLTFLoader()
+
+// loader4
+const gltfLoader4 = new GLTFLoader()
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvasshialian = document.querySelector('canvas.shilian')
+
+// canvas 2
+const canvasvaas = document.querySelector('canvas.vaas')
+
+// canvas 3
+const canvasvrouw = document.querySelector('canvas.vrouw')
+
+// canvas 4
+const canvasschilder = document.querySelector('canvas.schilder')
 
 // Scene
 const scene = new THREE.Scene()
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+// Scene 2
+const scene2 = new THREE.Scene()
 
-// Materials
+// Scene 3
+const scene3 = new THREE.Scene()
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+// Scene 4
+const scene4 = new THREE.Scene()
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+//timeline
+
+let lt = gsap.timeline()
+
+//obj group
+const objectGroup = new THREE.Group();
+scene.add(objectGroup);
+
+//obj group2
+const objectGroup2 = new THREE.Group();
+scene2.add(objectGroup2);
+
+//obj group3
+const objectGroup3 = new THREE.Group();
+scene3.add(objectGroup3);
+
+//obj group4
+const objectGroup4 = new THREE.Group();
+scene4.add(objectGroup4);
+
+// Shillian
+gltfLoader.load('shlian2.gltf', (gltf) =>{
+    gltf.scene.scale.set(3,3,3)
+    objectGroup.add(gltf.scene)
+})
+
+// vaas
+gltfLoader2.load('vaas7.gltf', (gltf2) =>{
+
+    gltf2.scene.scale.set(5.5,5.5,5.5)
+    objectGroup2.add(gltf2.scene)
+
+})
+
+// vrouw
+gltfLoader3.load('frauw.gltf', (gltf3) =>{
+
+    gltf3.scene.scale.set(4.5,4.5,4.5)
+    objectGroup3.add(gltf3.scene)
+
+})
+
+// schilder
+gltfLoader4.load('daandewit.gltf', (gltf4) =>{
+
+    gltf4.scene.scale.set(4,4,4)
+    objectGroup4.add(gltf4.scene)
+
+})
+
+
+
+// change color
+
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
+const pointLight = new THREE.SpotLight(0xffffff, 4 );
+pointLight.castShadow = true;
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
 
-/**
- * Sizes
- */
+// Lights2
+
+const pointLight2 = new THREE.SpotLight(0xffffff, 4 );
+pointLight2.castShadow = true;
+pointLight2.position.x = 2
+pointLight2.position.y = 3
+pointLight2.position.z = 4
+scene2.add(pointLight2)
+
+// Lights3
+
+const pointLight3 = new THREE.SpotLight(0xffffff, 2 );
+pointLight3.castShadow = true;
+pointLight3.position.x = 2
+pointLight3.position.y = 3
+pointLight3.position.z = 4
+scene3.add(pointLight3)
+
+// Lights4
+
+const pointLight4 = new THREE.SpotLight(0xffffff, 3 );
+pointLight4.castShadow = true;
+pointLight4.position.x = 2
+pointLight4.position.y = 3
+pointLight4.position.z = 4
+scene4.add(pointLight4)
+
+
+// Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -53,6 +152,8 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.outputEncoding = THREE.sRGBEncoding;
+
 })
 
 /**
@@ -65,22 +166,65 @@ camera.position.y = 0
 camera.position.z = 2
 scene.add(camera)
 
+// Base camera 2
+const camera2 = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera2.position.x = 0
+camera2.position.y = 0
+camera2.position.z = 2
+scene2.add(camera2)
+
+// Base camera 3
+const camera3 = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera3.position.x = 0
+camera3.position.y = 0
+camera3.position.z = 2
+scene3.add(camera3)
+
+// Base camera 4
+const camera4 = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera4.position.x = 0
+camera4.position.y = 0
+camera4.position.z = 2
+scene4.add(camera4)
+
 // Controls
 // const controls = new OrbitControls(camera, canvas)
 // controls.enableDamping = true
 
-/**
- * Renderer
- */
+
+ // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvasshialian,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+ // Renderer 2
+const renderer2 = new THREE.WebGLRenderer({
+    canvas: canvasvaas,
+    alpha: true
+})
+renderer2.setSize(sizes.width, sizes.height)
+renderer2.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+ // Renderer 3
+ const renderer3 = new THREE.WebGLRenderer({
+    canvas: canvasvrouw,
+    alpha: true
+})
+renderer3.setSize(sizes.width, sizes.height)
+renderer3.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+ // Renderer 4
+ const renderer4 = new THREE.WebGLRenderer({
+    canvas: canvasschilder,
+    alpha: true
+})
+renderer4.setSize(sizes.width, sizes.height)
+renderer4.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+ //Animate
 
 const clock = new THREE.Clock()
 
@@ -90,16 +234,53 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    //sphere.rotation.y = .5 * elapsedTime
 
     // Update Orbital Controls
     // controls.update()
 
+
+
     // Render
     renderer.render(scene, camera)
 
+    // Render2
+    renderer2.render(scene2, camera2)
+
+    // Render3
+    renderer3.render(scene3, camera3)
+
+    // Render4
+    renderer4.render(scene4, camera4)
+
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
+
 }
 
 tick()
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    objectGroup.rotation.x += 0.01;
+    objectGroup.rotation.y += 0.01;
+
+    objectGroup2.rotation.x += 0.01;
+    objectGroup2.rotation.y += 0.01;
+
+    objectGroup3.rotation.x += 0.01;
+    objectGroup3.rotation.y += 0.01;
+
+    objectGroup4.rotation.x += 0.01;
+    objectGroup4.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+animate();
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+// const controls2 = new OrbitControls(camera2, renderer1.domElement);
+
+
